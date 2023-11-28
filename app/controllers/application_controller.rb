66f289989e6ade183fdbbb4c7aee6d1d3aa74556
justinjkim <jjkim928@gmail.com>
2023-11-28 11:57:57 -0500

    11/28/23: render company name dynamically on the navbar
    Similar to the previous commit, this commit dynamically renders the
    company name on the navbar. I created a new method called `current_company`
    on the ApplicationController that essentially grabs the current_company
    associated with the current_user. However, I'm not a big fan of this
    approach, since I feel that this logic should either live on the model,
    or perhaps be extracted to an interaction. I believe that the controller
    should be primarily responsible for request logic. But, for the sake of
    time and following this tutorial, I'm going ahead with this change.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # is authenticated (logged in). If not, it will redirect the user
   # to the sign-in page.
   before_action :authenticate_user!, unless: :devise_controller?
+
+  private
+
+  def current_company
+    @current_company ||= current_user.company if user_signed_in? # user_signed_in? is from devise
+  end
+
+  helper_method :current_company
 end
