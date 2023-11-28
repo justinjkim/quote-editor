497b5bd7eb0adea80c9d600cef5f52bedb089802
justinjkim <jjkim928@gmail.com>
2023-11-28 11:08:30 -0500

    11/28/23: setup Devise user model and fixtures
    I placed a comment in the users.yml file, but because we are
    primarily interested in learning about Turbostreams and security
    for this tutorial, we don't have to include all the Devise modules
    quite yet.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+
+  devise :database_authenticatable, :registerable
+  # For now, we will comment out the below Devise modules since we just need
+  # users in our seeds and a way to login.
+  # :recoverable, :rememberable, :validatable
 end
