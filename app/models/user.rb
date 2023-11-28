6757c54d9b8a916f51013c6497692d0371a086a8
justinjkim <jjkim928@gmail.com>
2023-11-28 11:51:50 -0500

    11/28/23: render current_user name on navbar
    To dynamically render the logged in user's name on the navbar,
    we use Devise's `current_user` method (I believe this method itself
    is meta-programmed) to grab the user object. We also define a new
    method called `name` to split the user's email and grab the first
    part of their email address. Obviously this is just part of a tutorial
    and is a horrible way to actually grab the name of the user. In the
    future I will add a new column for the 'name' attribute.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  devise :database_authenticatable, :registerable
+  devise :database_authenticatable, :validatable
   # For now, we will comment out the below Devise modules since we just need
   # users in our seeds and a way to login.
-  # :recoverable, :rememberable, :validatable
+  # :recoverable, :rememberable, :registerable
 
   belongs_to :company
+
+  def name
+    email.split("@").first.capitalize
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,8 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "name" do
+    assert_equal "Accountant", users(:accountant).name
+  end
 end
