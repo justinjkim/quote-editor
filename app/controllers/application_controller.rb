6664a18900bc7ca21dea583ec414a9a584446451
justinjkim <jjkim928@gmail.com>
2023-11-28 11:39:54 -0500

    11/28/23: setup authentication for controllers
    We will use Devise's authentication to check if users are allowed
    to view certain pages. However, we want unauthenticated users to still
    be able to access the sign-in page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  # Devise's `authenticate_user!` method will first check if the user
+  # is authenticated (logged in). If not, it will redirect the user
+  # to the sign-in page.
+  before_action :authenticate_user!, unless: :devise_controller?
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "pages#home"
 
   resources :quotes # this will create the seven CRUD routes
 end
