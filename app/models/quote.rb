ce902c5896794fc367341ed8fd2d5370a866ba6b
justinjkim <jjkim928@gmail.com>
2023-11-28 12:18:26 -0500

    11/28/23: ensure signed stream names are different per company
    For security reasons, I need to ensure that users from one company
    cannot view the quotes from a different company, especially when
    the users are subscribed to the "quotes" stream. By passing in an
    array with the current_company in the array in the lambda for the
    stream, we ensure that someone from a different company won't be
    subscribed to the updates from a user in an another company.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,9 +5,9 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
-  # after_update_commit -> { broadcast_replace_later_to "quotes" }
-  # after_destroy_commit -> { broadcast_remove_to "quotes" }
-  # These three callbacks are equivalent to the following single line
-  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  # The signed stream name is generated from the array returned by the lambda
+  # that is the first argument of the `broadcasts_to` method. In general:
+  # 1. Users who share broadcastings should have the lambda return an array with the same values.
+  # 2. Users who shouldn't share broadcastings should have the lambda return an array with different values.
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 end
