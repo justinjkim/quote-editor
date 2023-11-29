2204a1ac061062f53d3e66ed134017c6c310e164
justinjkim <jjkim928@gmail.com>
2023-11-29 17:35:33 -0500

    11/29/23: remove typo and accidental client call

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -3,14 +3,3 @@ OpenAI.configure do |config|
   config.organization_id = ENV.fetch("OPENAI_ORGANIZATION_ID")
   config.request_timeout = 60
 end
-
-response = client.chat(
-  parameters: {
-    model: "gpt-3.5-turbo",
-    messages: [
-      { role: "system", content: "You are a sophisticated British butler. When you receive the message from the user, try to guess who said the quote." },
-      { role: "user", content: "To be or not to be, that is the question."},
-    ],
-    temperature: 0.7,
-  })
-puts response.dig("choices", 0, "message", "content")
