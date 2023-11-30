ea08ffcd8559f648a55df92810df7a5f77da1c39
justinjkim <jjkim928@gmail.com>
2023-11-30 08:54:15 -0500

    11/30/23: finish spike work for now
    While I was able to get ActiveInteraction to render a list of
    PORO versions of the ActiveRecord objects for the Quote model,
    I think it got too complicated trying to update the views for the
    various actions. For example, I successfully rendered the quotes
    as POROs for the index page, but things got messed up when it
    came to updating. So, I will conclude this spike work to utilize
    ActiveInteraction, and will switch to the strategy of just getting
    something to work. This probably means for my second pass I will
    be passing in ActiveRecord objects directly into the view (which
    I do not want to do), but I can refactor later, once I get the AI
    output to actually render on the page.

--- app/interactions/quotes/index.rb
+++ app/interactions/quotes/index.rb
@@ -21,7 +21,68 @@ class Quotes::Index < ActiveInteraction::Base
     end
     
     def quotes
-      company.quotes.ordered
+      company.quotes.ordered.map do |quote|
+        QuoteItem.new(quote: quote)
+      end
+    end
+  end
+  
+  class QuoteItem
+    def initialize(quote:)
+      @quote = quote
+    end
+    
+    def turbo_frame_tag_id
+      quote.persisted? ? "quote_#{quote.id}" : "new_quote"
+    end
+    
+    def name
+      quote.name
+    end
+    
+    def quote_path
+      Rails.application.routes.url_helpers.quote_path(quote)
+    end
+    
+    def edit_quote_path
+      Rails.application.routes.url_helpers.edit_quote_path(quote)
+    end
+    
+    def find_author
+      response =
+        client.chat(
+          parameters: {
+            model: "gpt-3.5-turbo",
+            messages: [
+                     {
+                       role: "user",
+                       content: name
+                     },
+                     {
+                       role: "system",
+                       content: "You are a sophisticated English butler. You will
+                  be directly given a quote by the user. The quote may be exactly
+                  worded correctly, or it may be paraphrased incorrectly. Do your
+                  best to guess who is the author of the quote. Then, give a fun
+                  fact about the author of the quote, no more than 20 words. If
+                  the author of the quote is a character, then give a fun fact
+                  about the character itself, not the author who created the
+                  character."
+                     }
+                   ],
+            temperature: 0.7
+          }
+        )
+      
+      puts response.dig("choices", 0, "message", "content")
+    end
+    
+    private
+    
+    attr_reader :quote
+    
+    def client
+      @client ||= OpenAI::Client.new
     end
   end
 end
