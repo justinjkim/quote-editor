f4fac6b0e6bea0d75635ca9d61284ce2ebdd69b2
justinjkim <jjkim928@gmail.com>
2023-12-03 22:56:55 -0500

    12/3/23: use different id for turbo-frame
    OMG IT FINALLY WORKS!! Looks like by moving the "Find Author" button
    outside of the initial turbo frame and that wrapping that in its own
    turbo frame worked. However, the problem is that when I update the
    quote, it renders a new "Find Author" button, so I'll need to fix that.
    But this is great progress for me.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -48,34 +48,32 @@ class QuotesController < ApplicationController
 
   def find_author
     # TODO: rescue Faraday::TimeoutError, or Net::ReadTimeout when API fails
-    # response =
-    #   client.chat(
-    #     parameters: {
-    #       model: "gpt-3.5-turbo",
-    #       messages: [
-    #                {
-    #                  role: "user",
-    #                  content: @quote.name
-    #                },
-    #                {
-    #                  role: "system",
-    #                  content: "You are a sophisticated English butler. You will
-    #               be directly given a quote by the user. The quote may be exactly
-    #               worded correctly, or it may be paraphrased incorrectly. Do your
-    #               best to guess who is the author of the quote. Then, give a fun
-    #               fact about the author of the quote, no more than 20 words. If
-    #               the author of the quote is a character, then give a fun fact
-    #               about the character itself, not the author who created the
-    #               character."
-    #                }
-    #              ],
-    #       temperature: 0.7
-    #     }
-    #   )
+    response =
+      client.chat(
+        parameters: {
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "user",
+              content: @quote.name
+            },
+            {
+              role: "system",
+              content: "You will be directly given a quote by the user. The
+                quote may be exactly worded correctly, or it may be paraphrased
+                incorrectly. Do your best to guess who is the author of the quote.
+                You should start off by saying 'It sounds like you may be quoting'.
+                Then, give a fun fact about the author of the quote, no more than
+                20 words. If the author of the quote is a character, then give a
+                fun fact about the character itself, not the author who created
+                the character."
+            }
+          ],
+          temperature: 0.7
+        }
+      )
 
-    # @ai_response = response.dig("choices", 0, "message", "content")
-
-    @ai_response = "hi there"
+    @ai_response = response.dig("choices", 0, "message", "content")
 
     respond_to do |format|
       format.html do
