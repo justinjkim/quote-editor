09c2884c1ff64c882c83ae05fad0b4250b98ab79
justinjkim <jjkim928@gmail.com>
2023-12-04 22:50:32 -0500

    12/4/23: add spec for find_author interaction
    In the spirit of having unit tests, I used some testing patterns
    at my previous employer for the FindAuthor interaction.

---
+++ spec/interactions/quotes/find_author_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.describe Quotes::FindAuthor do
+  subject { described_class }
+
+  let!(:company1) { Company.create!(name: "Google") }
+  let!(:quote1) { Quote.create!(name: "test", company: company1) }
+  let(:client_response1) { Hash.new }
+
+  context "instance" do
+    before do
+      allow_any_instance_of(OpenAI::Client).to receive(:chat).and_return(client_response1)
+      allow(client_response1).to receive(:dig).and_return("something")
+    end
+
+    it "knows its properties" do
+      outcome = subject.run(quote: quote1)
+
+      expect(outcome.quote).to eq(quote1)
+    end
+
+    it "returns a response from the API" do
+      outcome = subject.run(quote: quote1)
+
+      expect(outcome.result).to eq("something")
+    end
+
+    context "when API response takes too long" do
+      before do
+        allow_any_instance_of(OpenAI::Client).to receive(:chat).and_raise(Faraday::TimeoutError.new("Timeout"))
+      end
+
+      it "raises an exception" do
+        expect { subject.run(quote: quote1) }.to raise_error(Faraday::TimeoutError)
+      end
+    end
+  end
+end
